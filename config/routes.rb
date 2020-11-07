e226ccbff63b3937e983bf61b3232531a7030f5e
tashi1118 <tech.tashi1118.1991@gmail.com>
2020-11-07 12:02:58 +0900

    3-3まで

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  # ルート（/）にアクセスした時にタスクのindexを取得する記述
+  root to: 'tasks#index'
+  resources :tasks
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
