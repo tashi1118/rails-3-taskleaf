670ae281ea40919ac7ae1406c71fee8c3df2f5a7
tashi1118 <tech.tashi1118.1991@gmail.com>
2020-11-08 13:11:00 +0900

    多言語化設定

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,15 @@ module Taskleaf
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # デフォルトのlocaleを日本語(:ja)にする
+    config.i18n.default_locale = :ja
   end
 end
